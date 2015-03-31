3c85691f4676c164dab1979372c7a2a8e3986975
anwarMov <anwarMov@users.noreply.github.com>
2015-03-31 19:29:28 +0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := a400cg
+PRODUCT_RELEASE_NAME := ZENFONE 4
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -10,9 +10,22 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/asus/a400cg/device_a400cg.mk)
 
+UTC_DATE := $(shell date +%s)
+DATE := $(shell date +%Y%m%d)
+
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a400cg
+PRODUCT_DEVICE := ASUS_T00I
 PRODUCT_NAME := cm_a400cg
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := a400cg
 PRODUCT_MANUFACTURER := ASUS
+
+   PRODUCT_NAME=${PRODUCT_DEVICE} \
+   BUILD_NUMBER=${DATE} \
+   TARGET_DEVICE=${PRODUCT_DEVICE} \
+   BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_DEVICE}/${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
+   PRIVATE_BUILD_DESC="${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
+   PRODUCT_BRAND=${PRODUCT_BRAND} \
+   BUILD_UTC_DATE= \
+   PRODUCT_DEFAULT_LANGUAGE=en \
+   PRODUCT_DEFAULT_REGION=AS \
